0bba44c88a6c88c0f3c86dc268173f41a54407b2
stadia03 <gaurab.working@gmail.com>
2024-12-27 14:04:56 +0530

    cors fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,11 +8,30 @@ import cors from 'cors';
 import verifyToken from './middleware/verifyToken';
 
 
-
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
+
+app.use((req, res, next)  => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+     res.sendStatus(200);
+     return;
+  }
+  next();
+});
+
+app.options('*', cors()); // Preflight handling
+
+
+
 app.use(express.json());
 
 // app.use((req, res, next) => {
